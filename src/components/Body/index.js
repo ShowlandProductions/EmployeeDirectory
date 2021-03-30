1ba0f548f1e092132e8883e199833e106d290f60
Dominique Christopher <showlandproductions@gmail.com>
2021-03-30 16:55:36 -0700

    added /Body/index.js

---
+++ src/components/Body/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EmployeeArea from "../EmployeeArea/index.js";
+import "./Body.css";
+
+function Body (){
+    return (
+        <>
+        <EmployeeArea />
+        </>
+    );
+};
+// Exports Body
+export default Body;

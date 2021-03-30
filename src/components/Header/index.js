e01de57aa7825ffed0a903db3e6c86c8d61a01c7
Dominique Christopher <showlandproductions@gmail.com>
2021-03-30 16:56:23 -0700

    Added /Header/index.js & /Header/Header.css

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Use search bar or filter for more precise results</p>
+        </div>
+    )
+}
+export default Header;

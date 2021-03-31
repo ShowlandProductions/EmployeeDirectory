b19a8536962546aad4b843624aa51f27d9ba5854
Dominique Christopher <showlandproductions@gmail.com>
2021-03-30 17:30:51 -0700

    updated API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default{ 
     getUsers:function(){    
 
-        return axios.get("https://randomuser.me/api/?results=50");
+        return axios.get("https://randomuser.me/api/?results=5000");
     }
 };
